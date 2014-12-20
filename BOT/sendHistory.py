a69fd731792e3fc75700e7a983f53dae36b0e098
PangolinPaw <pangolinpaw@hotmail.co.uk>
2014-12-20 09:42:13 +0000

    Working on function that inly sends past 24hrs postHistory

--- BOT/sendHistory.py
+++ BOT/sendHistory.py
@@ -7,7 +7,7 @@ password = 'kitsune2211'
 
 def sendAll(to, content):
 	subject = 'KITSUNE: Full interaction history (manual request)'
-	message = "Below is a full list of bot interactions. They are in the format Timestamp | Username | Thier tweet | Bot's response\n\n"
+	message = "Below is the full history of Twitter interactions carried out by the KITSUNE bot."
 	timeStamp = 0
 	for line in content:
                 line = line.split('|')
@@ -19,8 +19,63 @@ def sendAll(to, content):
 			lineOne = '\n'
                 lineTwo = 'USER: \n%s\n' % line[1]
                 lineThree = 'TWEET: \n%s\n' % line[2]
-                lineFour = 'RESPONSE: \n%s\n\n' % line[3]
+                lineFour = 'RESPONSE: \n%s\n' % line[3]
 
 		message = '%s\n%s%s%s%s' % (message, lineOne, lineTwo, lineThree, lineFour)
 	
 	gmail.message(sender, password, to, subject, message)
+
+def sendRecent(to, allContent):
+	subject = 'KITSUNE: Recent interaction history.'
+	message = "Below are all the Twitter interactions from the past 24 hours."
+	timeStamp = 0
+	for line in content:
+                line = line.split('|')
+                pastTimeStamp = timeStamp
+                timeStamp = line[0]
+
+                if timeStamp != pastTimeStamp:
+                        lineOne = '-----%s-----\n' % timeStamp
+		else:
+			lineOne = '\n'
+                lineTwo = 'USER: \n%s\n' % line[1]
+                lineThree = 'TWEET: \n%s\n' % line[2]
+                lineFour = 'RESPONSE: \n%s\n' % line[3]
+
+		message = '%s\n%s%s%s%s' % (message, lineOne, lineTwo, lineThree, lineFour)
+	
+	gmail.message(sender, password, to, subject, message)
+	
+def getTime():
+	now = datetime.datwtime.now()
+	currentTime = now.strftime('%d.%m.%y %H:%M')
+	return currentTime
+
+def compareTime(timestamp, currentTime):
+# Check if the timestamp from postHistory is from less than 24hrs ago
+	# Separate timestamp strings into more easily compared lists
+	stampList = timestamp.split(' ')
+	stampDate = stampList(0)
+	stampDate = stampDate.split('.')
+
+	currentList = currentTime.split(' ')
+	currentDate = currentList(0)
+	currentDate = currentDate.split('.')
+
+	if stampDate(2) == currentDate(2):
+	# The years match
+		if stampDate(1) == currentDate(1):
+		# The months match
+			if int(stampDate(0)) == str(currentDate(0)) -1:
+			# The day is yesterday
+				check = True
+			else:
+			# The day isn't yesterday
+				check = False
+		else:
+		# The months don't match
+			check = False
+	else:
+	# The years don't match
+		check = False
+	return check
