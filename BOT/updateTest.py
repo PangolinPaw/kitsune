b5a162ed8391135ff8ed12640ee66b4ad76c243c
PangolinPaw <pangolinpaw@hotmail.co.uk>
2015-01-23 21:25:44 +0000

    Created test script to see if self-overwriting is possible

---
+++ BOT/updateTest.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# Test programatically pulling updates from GitHub & self-overwriting
+# This is version 1
+
+import os
+
+os.system('git pull origin master')
+print 'pull complete'
+
